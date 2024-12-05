89496ccf2e75ca3b57c3d06e09e1b02adba857a5
zekehuntergreen <zeke.huntergreen@guardian.co.uk>
2024-12-05 13:17:54 +0000

    pr suggestions

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,28 +3,30 @@ use std::time::Instant;
 
 const INPUT: &[u8] = include_bytes!("input.txt");
 
-fn parse(array: &[u8]) -> i32 {
+fn parse_two_digit_number(array: &[u8]) -> i32 {
     if array.len() == 1 {
         return (array[0] - b'0') as i32;
     }
-    return (array[0] - b'0') as i32 * 10 + (array[1] - b'0') as i32;
+    (array[0] - b'0') as i32 * 10 + (array[1] - b'0') as i32
 }
 
 fn report_is_safe(report: &Vec<i32>) -> bool {
     let asc = report[1] > report[0];
-    report
-        .windows(2)
-        .find(|w| {
-            let diff = if asc { w[1] - w[0] } else { w[0] - w[1] };
-            diff < 1 || diff > 3
-        })
-        .is_none()
+    !report.windows(2).any(|w| {
+        let diff = if asc { w[1] - w[0] } else { w[0] - w[1] };
+        diff < 1 || diff > 3
+    })
 }
 
 fn part1() -> usize {
     INPUT
         .split(|&b| b == b'\n')
-        .map(|report| report.split(|&b| b == b' ').map(parse).collect())
+        .map(|report| {
+            report
+                .split(|&b| b == b' ')
+                .map(parse_two_digit_number)
+                .collect()
+        })
         .filter(report_is_safe)
         .count()
 }
@@ -32,13 +34,15 @@ fn part1() -> usize {
 fn part2() -> i32 {
     let mut num_safe = 0;
     for report in INPUT.split(|&b| b == b'\n') {
-        let report: Vec<i32> = report.split(|&b| b == b' ').map(parse).collect();
+        let report: Vec<i32> = report
+            .split(|&b| b == b' ')
+            .map(parse_two_digit_number)
+            .collect();
         let report_length = report.len();
         if report
             .into_iter()
-            .combinations((report_length - 1).try_into().unwrap())
-            .find(report_is_safe)
-            .is_some()
+            .combinations(report_length - 1)
+            .any(|report: Vec<i32>| report_is_safe(&report))
         {
             num_safe += 1
         }
